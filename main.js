156c699b65a4be3c30accda87cfc97b4d4ecac62
VRosa-jpg <vixkirosa@gmail.com>
2024-10-22 14:44:22 -0400

    Update

--- main.js
+++ main.js
@@ -61,31 +61,96 @@
 // const total = product1.calculateTotal(salesTax)
 // console.log(`Total price with tax: $${total.toFixed(2)}`)
 
+class MathEq {
+    constructor(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+
+    }
+
+    add () {
+        return this.num1 + this.num2;
+    }
+}
+
+class Multiply extends MathEq {
+    constructor(num1, num2, num3) {
+        super(num1, num2)
+        this.num3 = num3;
+    }
+
+    mult() {
+        return this.num1 * this.num2 * this.num3
+    }
+
+    add() {
+        let addedUp = (num1 + num2) * num3
+        return addedUp
+    }
+}
+
+let equation = new MathEq(2, 4)
+console.log(MathEq)
+// JS Classes
+// Classes are blueprint to create objects
+//  Has three things:
+//      -Properties - intialized when you create an object using a constructor
+//      -Constructor to initialize those properties
+//      -Methods to manipulate those properties
+
 
 // Lab Part
 
-const adventurer = {
-    name: 'Robin',
-    health: 10,
-    inventory:['Sword', 'Potion', 'Artifact'],
-    companion: {
-        name: "Leo",
-        type: "cat",
-        companion: {
-            name: 'Frank',
-            type: 'mouse'
-        }
-    },
+// Part 1
+// const adventurer = {
+//     name: 'Robin',
+//     health: 10,
+//     inventory:['Sword', 'Potion', 'Artifact'],
+//     companion: {
+//         name: "Leo",
+//         type: "cat",
+//         companion: {
+//             name: 'Frank',
+//             type: 'mouse'
+//         }
+//     },
 
-    roll (mod = 0) {
-        const result = Math.floor(Math.random() * 21) + mod;
-        console.log(`${this.name} rolled a ${result}`)
-    }
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 21) + mod;
+//         console.log(`${this.name} rolled a ${result}`)
+//     }
 
-}
-
-// for (let i = 0; i < adventurer.inventory.length; i++) {
-//     console.log(adventurer.inventory[i])
 // }
 
-adventurer.roll();
+// // for (let i = 0; i < adventurer.inventory.length; i++) {
+// //     console.log(adventurer.inventory[i])
+// // }
+
+// adventurer.roll();
+
+// // Part 2
+
+// class Character {
+//     constructor (name) {
+//         this.name = name;
+//         this.health = 100;
+//         this.inventory = [];
+//         this.companion = null;
+//     }
+
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 21) + mod;
+//         console.log(`${this.name} rolled a ${result}`)
+//     }
+// }
+
+// const robin = new Character("Robin");
+// robin.inventory = ["Sword", "Potion", "Artifact"];
+
+// // Robins companions into object
+// robin.companion = new Character("Cat");
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["Small hat", "sunglasses"]
+
+// robin.companion.roll()
